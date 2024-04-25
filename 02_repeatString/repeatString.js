d8e32813568024c3acd6603aedf01a8f0ab790a6
Ian Watts <164903808+imwatts93@users.noreply.github.com>
2024-04-25 13:05:17 +0900

    Add conditionals for negative input

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+let stringToRepeat = ""
 
+let numOfTimes = 0
+
+let i = 0
+
+const repeatString = function(stringToRepeat, numOfTimes) {
+    let repeatedString = ''
+    if (numOfTimes < 0) {
+        return 'ERROR'
+    }
+
+    else if (numOfTimes === 1) {
+        return stringToRepeat
+    }
+
+    else
+    for (let i =0; i < numOfTimes; i++) {
+    repeatedString += stringToRepeat;
+    } 
+    return repeatedString;
 };
 
 // Do not edit below this line
