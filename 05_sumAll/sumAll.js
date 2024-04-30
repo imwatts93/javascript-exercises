b4495cc4c5a8e47b90be88a60262498b7988cd6e
Ian Watts <164903808+imwatts93@users.noreply.github.com>
2024-04-30 17:09:40 +0900

    Return ERROR with non-numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,15 @@
 const sumAll = function(num1, num2) {
-    if (num1 > num2) {
-        const temp = num1
-        num1 = num2
-        num2 = temp
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
     }
-
-
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
     let finalSum = 0
     for (i = num1; i <= num2; i++) {
         finalSum += i;
