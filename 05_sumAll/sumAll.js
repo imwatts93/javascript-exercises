2c2a333334ce440e467ce2a1e3740be2e653e3cf
Ian Watts <164903808+imwatts93@users.noreply.github.com>
2024-04-30 17:01:29 +0900

    Make it work if larger num goes first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 > num2) {
+        const temp = num1
+        num1 = num2
+        num2 = temp
+    }
 
+
+    let finalSum = 0
+    for (i = num1; i <= num2; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
