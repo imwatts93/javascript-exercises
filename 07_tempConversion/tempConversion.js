fe2ccf4765773e302569be5db64cfa8b08d6720d
Ian Watts <164903808+imwatts93@users.noreply.github.com>
2024-05-01 12:34:07 +0900

    Test one passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let cel = Number((fahrenheit - 32) * (5/9).toFixed(1));
+  return cel
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let far = Number((celcius * (9/5) + 32).toFixed(1));
+  return far
 };
 
 // Do not edit below this line
